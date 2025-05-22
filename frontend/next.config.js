97523d61fc120bb337ae3e816853bbb8eae2dbc0
spacelord16 <deshpande.aditya16@gmail.com>
2025-05-22 14:03:35 -0700

    fixed vercel deployment v2

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,14 +1,20 @@
 /** @type {import('next').NextConfig} */
+const path = require("path");
+
 const nextConfig = {
   reactStrictMode: true,
-  experimental: {
-    serverActions: true,
-  },
   webpack: (config) => {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
     };
+
+    // Add alias for src directory
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      "@": path.join(__dirname, "src"),
+    };
+
     return config;
   },
 };
